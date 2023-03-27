c81c27fc0f28c3185060531f8daa1898035b4a61
KanjiEater <kanjieat3r@gmail.com>
2023-03-27 05:39:40 -0500

    Anki now adds cards automatically

--- anki.sh
+++ anki.sh
@@ -9,5 +9,5 @@ echo "Connecting to AnkiConnect via $ANKICONNECTURL"
 echo "Mapping: $MAPPING";
 echo "TSV: $FOLDER/srs_export/$NAME.tsv";
 
-# python -m subs2cia srs -i "$FOLDER/$NAME.m4b" "$FOLDER/$NAME.srt" -d "$FOLDER/srs_export" -p 100
-python ./anki-importer.py --anki-connect-url="$ANKICONNECTURL" --path "$FOLDER" --mapping "$MAPPING" --name "$NAME" --anki-media-dir "$ANKI_MEDIA_DIR"
+python -m subs2cia srs -i "$FOLDER/$NAME.m4b" "$FOLDER/$NAME.srt" -d "$FOLDER/srs_export" -p 100
+python ./anki-importer.py --anki-connect-url="$ANKICONNECTURL" --path "$FOLDER/" --mapping "$MAPPING" --name "$NAME" --anki-media-dir "$ANKI_MEDIA_DIR"
