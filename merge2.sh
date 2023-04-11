2206b77c3579aefc3ce74af22bcd695e76e9513d
KanjiEater <kanjieat3r@gmail.com>
2023-04-11 15:24:52 -0500

    added alternative batch merge command that grabs files itself

--- merge2.sh
+++ merge2.sh
@@ -11,10 +11,18 @@ do
   # Get the audiobook name
   audiobook_name="$(basename "${audiobook_folder}")"
 
+  # Get the list of mp4 files in the audiobook folder
+  mp4_files=$(ls -1v "${audiobook_folder}"*.mp4)
+
+  # Replace audiobooks_path with "./" in each mp4 file path
+  mp4_files=$(echo "$mp4_files" | sed "s|${audiobook_folder}|./|g")
+
+  # Wrap each mp4 file path in quotes
+  mp4_files=$(echo "$mp4_files" | awk '{printf "\"%s\" ", $0}')
+
   # Run the m4b-tool Docker container to merge the MP4 files
-  docker_cmd="docker run -it --rm -u $(id -u):$(id -g) -v \"${audiobook_folder}\":/mnt sandreas/m4b-tool:latest merge \"./\" --output-file \"./${audiobook_name}.m4b\" --jobs $(nproc --all)"
+  docker_cmd="docker run -it --rm -u $(id -u):$(id -g) -v \"${audiobook_folder}\":/mnt sandreas/m4b-tool:latest merge ${mp4_files} --output-file \"./${audiobook_name}.m4b\" --jobs $(nproc --all)"
 
   echo "$docker_cmd"
   eval "$docker_cmd"
 done
-
