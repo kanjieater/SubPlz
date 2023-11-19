aa31d8fb1c332aaa19494162f41fdc140373c46d
ym555 <mohamed.yoyo100@gmail.com>
2023-11-19 18:29:47 +0200

    h

--- src/main2.py
+++ src/main2.py
@@ -367,7 +367,7 @@ def transcribe(
             break
         n = len(i)
         while n > current and e < len(x):
-            print(f"words{s}": ''.join([i['word'] for i in x[s:e]]))
+            print(f"words{s}", ''.join([i['word'] for i in x[s:e]]))
             current += len(x[e]['word'])
             e += 1
         # print(i, ''.join([i['word'] for i in x[s:e]]))
