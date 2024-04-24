38a5eee2638b10c84912edeb55a33a19a2ad9252
ym555 <mohamed.yoyo100@gmail.com>
2024-04-24 18:09:59 +0200

    l

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ from types import MethodType
 def is_notebook() -> bool:
     try:
         shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
+        if shell == 'ZMQInteractiveShell' or shell == 'google.colab._shell':
             return True   # Jupyter notebook or qtconsole
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
         else:
-            return False  # Other type (?)
+            return True  # Other type (?)
     except NameError:
         return False      # Probably standard Python interpreter
 
