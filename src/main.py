274c9b6444722430bfed6c74c7afdeec523b5b1c
ym555 <mohamed.yoyo100@gmail.com>
2024-05-16 07:23:40 +0300

    easier to just do it by yourself

--- src/main.py
+++ src/main.py
@@ -331,10 +331,10 @@ def print_batches(batches):
                 else:
                     c.append(basename(t.path))
             c = '\n'.join(c)
-            h.append([a, c, score/100 if score is not None else None])
+            h.append([a, c, format(score/100, '.2%') if score is not None else '?'])
 
     # floatfmt doesn't work?
-    print(tabulate(h, headers=["Audio", "Text", "Score"], tablefmt='rst', floatfmt=".2%", missingval='?'))
+    print(tabulate(h, headers=["Audio", "Text", "Score"], tablefmt='rst'))
 
 
 def to_epub():
@@ -404,17 +404,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Match audio to a transcript")
     parser.add_argument( "--audio", nargs="+", required=True, help="list of audio files to process (in the correct order)")
     parser.add_argument("--text", nargs="+", required=True, help="path to the script file")
+
     parser.add_argument("--model", default="tiny", help="whisper model to use. can be one of tiny, small, large, huge")
+    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to do inference on")
+    parser.add_argument("--threads", type=int, default=multiprocessing.cpu_count(), help=r"number of threads")
     parser.add_argument("--language", default=None, help="language of the script and audio")
+    parser.add_argument("--local-only", default=False, help="Don't download outside models", action=argparse.BooleanOptionalAction)
+
     parser.add_argument("--progress", default=True,  help="progress bar on/off", action=argparse.BooleanOptionalAction)
     parser.add_argument("--overwrite", default=False,  help="Overwrite any destination files", action=argparse.BooleanOptionalAction)
+
     parser.add_argument("--use-cache", default=True, help="whether to use the cache or not", action=argparse.BooleanOptionalAction)
     parser.add_argument("--cache-dir", default="AudiobookTextSyncCache", help="the cache directory")
     parser.add_argument("--overwrite-cache", default=False, action=argparse.BooleanOptionalAction, help="Always overwrite the cache")
-    parser.add_argument("--threads", type=int, default=multiprocessing.cpu_count(), help=r"number of threads")
-    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to do inference on")
-    parser.add_argument("--dynamic-quantization", "--dq", default=False, help="Use torch's dynamic quantization (cpu only)", action=argparse.BooleanOptionalAction)
+
     parser.add_argument('--quantize', default=True, help="use fp16 on gpu or int8 on cpu", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--dynamic-quantization", "--dq", default=False, help="Use torch's dynamic quantization (cpu only)", action=argparse.BooleanOptionalAction)
 
     parser.add_argument("--faster-whisper", default=True, help='Use faster_whisper, doesn\'t work with hugging face\'s decoding method currently', action=argparse.BooleanOptionalAction)
     parser.add_argument("--fast-decoder", default=False, help="Use hugging face's decoding method, currently incomplete", action=argparse.BooleanOptionalAction)
@@ -434,19 +439,19 @@ if __name__ == "__main__":
     parser.add_argument("--compression_ratio_threshold", type=float, default=2.4, help="if the gzip compression ratio is higher than this value, treat the decoding as failed")
     parser.add_argument("--logprob_threshold", type=float, default=-1.0, help="if the average log probability is lower than this value, treat the decoding as failed")
     parser.add_argument("--no_speech_threshold", type=float, default=0.6, help="if the probability of the <|nospeech|> token is higher than this value AND the decoding has failed due to `logprob_threshold`, consider the segment as silence")
-    parser.add_argument("--word_timestamps", default=False, help="(experimental) extract word-level timestamps and refine the results based on them", action=argparse.BooleanOptionalAction)
+
     parser.add_argument("--prepend_punctuations", type=str, default="\"\'“¿([{-『「（〈《〔【｛［‘“〝※", help="if word_timestamps is True, merge these punctuation symbols with the next word")
     parser.add_argument("--append_punctuations", type=str, default="\"\'・.。,，!！?？:：”)]}、』」）〉》〕】｝］’〟／＼～〜~", help="if word_timestamps is True, merge these punctuation symbols with the previous word")
     parser.add_argument("--nopend_punctuations", type=str, default="うぁぃぅぇぉっゃゅょゎゕゖァィゥェォヵㇰヶㇱㇲッㇳㇴㇵㇶㇷㇷ゚ㇸㇹㇺャュョㇻㇼㇽㇾㇿヮ…\u3000\x20", help="TODO")
+
+    parser.add_argument("--word_timestamps", default=False, help="(experimental) extract word-level timestamps and refine the results based on them", action=argparse.BooleanOptionalAction)
     parser.add_argument("--highlight_words", default=False, help="(requires --word_timestamps True) underline each word as it is spoken in srt and vtt", action=argparse.BooleanOptionalAction)
     parser.add_argument("--max_line_width", type=int, default=None, help="(requires --word_timestamps True) the maximum number of characters in a line before breaking the line")
     parser.add_argument("--max_line_count", type=int, default=None, help="(requires --word_timestamps True) the maximum number of lines in a segment")
     parser.add_argument("--max_words_per_line", type=int, default=None, help="(requires --word_timestamps True, no effect with --max_line_width) the maximum number of words in a segment")
+
     parser.add_argument("--output-dir", default=None, help="Output directory, default uses the directory for the first audio file")
     parser.add_argument("--output-format", default='srt', help="Output format, currently only supports vtt and srt")
-    parser.add_argument("--local-only", default=False, help="Don't download outside models", action=argparse.BooleanOptionalAction)
-
-    # parser.add_argument("--split-script", default="", help=r"the regex to split the script with. for monogatari it is something like ^\s[\uFF10-\uFF19]*\s$")
 
     args = parser.parse_args().__dict__
     tqdm.__init__ = partialmethod(tqdm.__init__, disable=not args.pop('progress'))
@@ -459,32 +464,33 @@ if __name__ == "__main__":
     model, device = args.pop("model"), args.pop('device')
     if device == 'cuda' and not torch.cuda.is_available():
         device = 'cpu'
-    print(f"We're using {device}")
+    print(f"Using device: {device}")
+
     overwrite, overwrite_cache = args.pop('overwrite'), args.pop('overwrite_cache')
     cache = Cache(model_name=model, enabled=args.pop("use_cache"), cache_dir=args.pop("cache_dir"),
                   ask=not overwrite_cache, overwrite=overwrite_cache,
                   memcache={})
 
-    faster_whisper = args.pop('faster_whisper')
-    local_only = args.pop('local_only')
-    quantize = args.pop("quantize")
+    faster_whisper, local_only, quantize = args.pop('faster_whisper'), args.pop('local_only'), args.pop('quantize')
+    fast_decoder, overlap, batches = args.pop('fast_decoder'), args.pop("fast_decoder_overlap"), args.pop("fast_decoder_batches")
+    dq = args.pop('dynamic_quantization')
     if faster_whisper:
         model = WhisperModel(model, device, local_files_only=local_only, compute_type='float32' if not quantize else ('int8' if device == 'cpu' else 'float16'), num_workers=threads)
         model.transcribe2 = model.transcribe
         model.transcribe = MethodType(faster_transcribe, model)
     else:
-        model = whisper.load_model(model).to(device)
-        if quantize and device != 'cpu':
+        model = whisper.load_model(model, device)
+        args['fp16'] = quantize and device != 'cpu'
+        if args['fp16']:
             model = model.half()
+        elif dq:
+            ptdq_linear(model)
 
-    if args.pop('dynamic_quantization') and device == "cpu" and not faster_whisper:
-        ptdq_linear(model)
+        if fast_decoder:
+            args["overlap"] = overlap
+            args["batches"] = batches
+            modify_model(model)
 
-    overlap, batches = args.pop("fast_decoder_overlap"), args.pop("fast_decoder_batches")
-    if args.pop("fast_decoder") and not faster_whisper:
-        args["overlap"] = overlap
-        args["batches"] = batches
-        modify_model(model)
 
     print("Loading...")
     streams = [(os.path.basename(f), *AudioStream.from_file(f)) for f in args.pop('audio')]
